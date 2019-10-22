1875445c22764d99d3b16757b494b22d813c8dba
Unknown <zakiyfadhilmuhsin@gmail.com>
2019-10-23 03:53:47 +0700

    changed hostdomain

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import router from 'src/router/routes'
 
-export const hostDomain = 'https://api.needherbal.web.id'
+export const hostDomain = 'https://api-test.needherbal.web.id'
 //export const hostDomain = 'http://apigateway.test'
 
 export const apiDomain = hostDomain
