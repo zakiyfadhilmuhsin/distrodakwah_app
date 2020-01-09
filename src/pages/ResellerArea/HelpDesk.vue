44c5f24e4f908f058d9303370a4eb126eea9ce6e
Zakiy Fadhil Muhsin <zakiyfadhilmuhsin@gmail.com>
2020-01-09 08:04:27 +0700

    Add Ticket WA Feature

--- src/pages/ResellerArea/HelpDesk.vue
+++ src/pages/ResellerArea/HelpDesk.vue
@@ -15,52 +15,43 @@
       </q-toolbar>
     </q-header>
 
-    <q-footer class="mobile-layout-on-desktop">
-      <center>
-        <q-tabs
-          dense
-          class="bg-white text-black"
-          style="border-top: 2px solid #eeeeee;"
-          animated
-          swipeable
-          align="justify"
-        >
-          <q-route-tab
-            icon="home"
-            to="/"
-            style="text-transform: capitalize; font-family: 'Open Sans'"
-          ><span style="font-size: 10px;">Beranda</span></q-route-tab>
-          <q-route-tab
-            icon="receipt"
-            to="/orderList"
-            style="text-transform: capitalize; font-family: 'Open Sans'"
-          ><span style="font-size: 10px;">Pesanan</span></q-route-tab>
-          <q-route-tab
-            icon="local_mall"
-            to="/cart"
-            style="text-transform: capitalize; font-family: 'Open Sans'"
-          ><span style="font-size: 10px;">Keranjang</span><q-badge color="red" text-color="white" floating v-if="totalCartItem !== null"><b>{{ totalCartItem }}</b></q-badge></q-route-tab>
-          <q-route-tab
-            icon="account_circle"
-            to="/dashboard"
-            style="text-transform: capitalize; font-family: 'Open Sans'"
-          ><span style="font-size: 10px;">Profil</span></q-route-tab>
-          <!-- <q-route-tab
-            icon="verified_user"
-            to="/storefront"
-            style="text-transform: capitalize; font-family: 'Open Sans'"
-          ><span style="font-size: 10px;">Support</span></q-route-tab> -->
-        </q-tabs>
-      </center>
-    </q-footer>
-
     <q-page-container class="mobile-layout-on-desktop">
       <q-page class="bg-white">
         <div class="row order-list-section">
-          <div class="col">
-              <div class="q-px-lg">
-                <iframe title="Feedback Form" class="freshwidget-embedded-form" id="freshwidget-embedded-form" src="https://distrodakwah.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&screenshot=No&attachFile=no" scrolling="yes" height="530px" width="100%" frameborder="0" ></iframe>
-              </div>
+          <div class="col q-pa-lg">
+            <q-card bordered class="shadow-1">
+              <q-card-section>
+                <div class="q-pa-xs">
+                    <div style="font-size: 14px; margin: 0 0 5px 0; font-weight: bold">Tipe Bantuan :</div>
+                    <q-select outlined dense color="orange-8" v-model="typeHelp" :options="typeHelpOption" :options-dense="true" @input="problemSelectionGenerate" />
+                </div>
+                <div class="q-pa-xs" v-if="typeHelp !== ''">
+                    <div style="font-size: 14px; margin: 0 0 5px 0; font-weight: bold" v-if="typeHelp !== 'Saran dan Masukkan'">Pilihan Masalah :</div>
+                    <div style="font-size: 14px; margin: 0 0 5px 0; font-weight: bold" v-else>Pilihan Saran :</div>
+                    <q-select outlined dense color="orange-8" v-model="problemSelection" :options="problemSelectionOpt" :options-dense="true" />
+                </div>
+                <div class="q-pa-xs">
+                    <div style="font-size: 14px; margin: 0 0 5px 0; font-weight: bold">Judul :</div>
+                    <q-input outlined dense color="orange-8" type="text" v-model="titleHelp" placeholder="Silahkan Masukkan Judul" />
+                </div>
+                <div class="q-pa-xs">
+                    <div style="font-size: 14px; margin: 0 0 5px 0; font-weight: bold">Deskripsi/Penjelasan :</div>
+                    <q-input dense outlined color="orange-8" type="textarea" rows="8" placeholder="Isi Masukkan Anda Disini.." v-model="entryHelp" />
+                </div>
+                <!-- <div class="q-pa-xs">
+                </div>
+                <div class="q-pa-xs">
+                </div> -->
+              </q-card-section>
+            </q-card>
+            <br/>
+            <q-card bordered class="shadow-1">
+              <q-card-section>
+                <div class="q-pa-xs">
+                    <q-btn @click="sendTicket" flat class="bg-orange-8 text-white text-capitalize full-width">Kirim</q-btn>
+                </div>
+              </q-card-section>
+            </q-card>
           </div>
         </div>
       </q-page>
@@ -75,125 +66,44 @@
 </style>
 
 <script>
-import axios from 'axios';
-import { getOrderUrl, showCustomerUrl, totalCartItemUrl, getHeader } from 'src/config';
-import flatPickr from 'vue-flatpickr-component';
-import 'flatpickr/dist/flatpickr.css';
-// Loading
-import { QSpinnerPuff } from 'quasar'
+import { openURL } from 'quasar';
 
 export default {
-  name: 'OrderList',
-  components: {
-    flatPickr
-  },
+  name: 'HelpDesk',
   data () {
     return {
-      orderTab: 'waiting_payment',
-      searchInvoice: '',
-      dateFilter: '',
-      orderData: [],
-      // Total Count Cart Item
-      totalCartItem: null,
+      typeHelp: '',
+      typeHelpOption: ['Sistem Web/Aplikasi','Pesanan','Saran dan Masukkan'],
+      problemSelection: '',
+      problemSelectionOpt: [],
+      titleHelp: '',
+      entryHelp: '',
+      user: null,
     }
   },
   created () {
     this.user = JSON.parse(window.localStorage.getItem('profileUser'));
-    if(this.$route.params.status){
-      this.orderTab = this.$route.params.status;
-    }else{
-      this.getOrder();
-    }
-  },
-  mounted () {
-    // Get Total Cart Item
-    axios.get( totalCartItemUrl + '/' + this.user.id, { headers: getHeader() } )
-      .then(response => {
-        console.log(response)
-
-        if (response.status === 200) {
-          this.totalCartItem = response.data.data;
-        }
-
-      })
-      .catch(error => {
-        if (error.response) {
-          console.log(error.response)
-        }
-      })
   },
   methods: {
-    getOrder () {
-      
-      this.$q.loading.show({
-        spinner: QSpinnerPuff,
-        spinnerColor: 'black',
-        spinnerSize: 50,
-        backgroundColor: 'grey',
-        message: '<b>Mohon Tunggu..</b>',
-        messageColor: 'black'
-      })
+    problemSelectionGenerate() {
+      this.problemSelection = '';
+      this.problemSelectionOpt = [];
 
-      this.orderData = [];
-
-      let formatUrl = null;
-
-      if(this.searchInvoice !== ''){
-        formatUrl = getOrderUrl + '/' + this.user.id + '?status=' + this.orderTab + '&search=' + this.searchInvoice;
-      }else if(this.dateFilter !== ''){
-        formatUrl = getOrderUrl + '/' + this.user.id + '?status=' + this.orderTab + '&date=' + this.dateFilter;
-      }else{
-        formatUrl = getOrderUrl + '/' + this.user.id + '?status=' + this.orderTab;
+      switch(this.typeHelp){
+        case 'Sistem Web/Aplikasi':
+          this.problemSelectionOpt.push('Masalah pada Web/Aplikasi');
+          break;
+        case 'Pesanan':
+          this.problemSelectionOpt.push('Kiriman', 'Komplain Masalah Produk', 'Tidak Sesuai Pesanan', 'Pembayaran');
+          break;
+        case 'Saran dan Masukkan':
+          this.problemSelectionOpt.push('Produk', 'Pelayanan', 'Sistem Web/Aplikasi', 'Saran dan Masukkan lain');
+          break;
+        default:
       }
-
-      axios.get( formatUrl, { headers: getHeader() } )
-        .then(response => {
-          console.log(response)
-
-          if (response.status === 200) {
-
-            this.$q.loading.hide()
-            
-            //this.orderData = response.data.data;
-            for(let i=0; i<response.data.data.length; i++){
-              this.orderData.push({
-                id: response.data.data[i].id,
-                invoice: response.data.data[i].invoice,
-                status: response.data.data[i].status,
-                grand_total: response.data.data[i].grand_total,
-                created_at: response.data.data[i].created_at,
-                detail_customer: [],
-              });
-
-              axios.get(showCustomerUrl + '/' + response.data.data[i].customer_address_id, { headers: getHeader() }).then(response => {
-                
-                this.orderData[i].detail_customer.push({
-                  customer_name: response.data.data.customer_name,
-                });
-
-              }).catch(error => {
-                if (error.response) {
-                  console.log(error.response)
-                }
-              })
-
-            }
-          }
-
-        })
-        .catch(error => {
-          if (error.response) {
-            console.log(error.response)
-          }
-        })
-
     },
-    formatPrice(value) {
-        let val = (value/1).toFixed(0).replace('.', ',')
-        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-    },
-    goBack() {
-      this.$router.go(-1);
+    sendTicket() {
+      openURL("https://wa.me/62895804141559?text===*Tiket%20Bantuan*==%0A"+ "%0A*Nama%20User:*%20" + this.user.name +"%0A*Email%20User:*%20" + this.user.email + "%0A*No%20HP%20User:*%200" + this.user.phone + "%0A*Tipe%20Bantuan:*%20" + this.typeHelp + "%0A*Problem/Saran:*%20" + this.problemSelection + "%0A*Judul:*%20" + this.titleHelp + "%0A*Deskripsi/Penjelasan:*%20" + this.entryHelp + "%20");
     }
   }
 }
