61af01dc1492f5ef1f5218b4d2ca02d963b385c7
fsidiqs <fsidiqs@hotmail.co.id>
2020-04-30 05:27:36 +0700

    new algorithm

--- src/router/index.js
+++ src/router/index.js
@@ -1,67 +1,63 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import { LocalStorage } from 'quasar'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { LocalStorage } from "quasar";
 
-import routes from './routes'
+import routes from "./routes";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 /*
  * If not building with SSR mode, you can
  * directly export the Router instantiation
  */
 
-export default function (/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ x: 0, y: 0 }),
-    routes,
+export default function(/* { store, ssrContext } */) {
+	const Router = new VueRouter({
+		scrollBehavior: () => ({ x: 0, y: 0 }),
+		routes,
 
-    // Leave these as is and change from quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    mode: 'history',
-    base: '/'
-  })
-  
+		// Leave these as is and change from quasar.conf.js instead!
+		// quasar.conf.js -> build -> vueRouterMode
+		// quasar.conf.js -> build -> publicPath
+		mode: "history",
+		base: "/"
+	});
 
-  Router.beforeEach((to, from, next) => {
-    console.log(JSON.parse(localStorage.getItem('profileUser')).email);
-    
-  if (JSON.parse(localStorage.getItem('profileUser')).email == "fssalviro@gmail.com" || to.fullPath != "/maintenance") {
-    next();
-  } else 
-  {
-    next('/maintenance');
-  } 
-    
-    if (to.matched.some(item => item.meta.requiresAuth)) {
-
-      // const authUser = JSON.parse(window.localStorage.getItem('authUser'))
-
-      if (LocalStorage.has('authUser')) {
-
-        if (LocalStorage.getItem('authUser') && LocalStorage.getItem('authUser').access_token !== 'undefined') {
-          
-          //console.log(authUser.access_token)
-          next()
-        
-        }
-
-      } else {
-
-        next({
-          path: '/login',
-          query: { redirect: to.fullPath }
-        })
-        
+	Router.beforeEach((to, from, next) => {
+		console.log(JSON.parse(localStorage.getItem("profileUser"))==null);
+    if(	JSON.parse(localStorage.getItem("profileUser")) != null) {
+      if(		JSON.parse(localStorage.getItem("profileUser")).email ==
+      "fssalviro@gmail.com") {
+        next();
       }
-
-    } else {
-
+    } else if(to.fullPath == "/maintenance" || to.fullPath == "/login-testing") {
       next()
-    
+    } else {
+      next('/maintenance')
     }
-  })
-  
-  return Router
+	
+
+		if (to.matched.some(item => item.meta.requiresAuth)) {
+			// const authUser = JSON.parse(window.localStorage.getItem('authUser'))
+
+			if (LocalStorage.has("authUser")) {
+				if (
+					LocalStorage.getItem("authUser") &&
+					LocalStorage.getItem("authUser").access_token !== "undefined"
+				) {
+					//console.log(authUser.access_token)
+					next();
+				}
+			} else {
+				next({
+					path: "/login",
+					query: { redirect: to.fullPath }
+				});
+			}
+		} else {
+			next();
+		}
+	});
+
+	return Router;
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -23,7 +23,7 @@ import UpdatePassword from "pages/Auth/UpdatePassword";
 import UpdatePasswordHasToken from "pages/Auth/UpdatePasswordHasToken";
 import MaintenancePage from "pages/Maintenance";
 
-// import LoginTesting from "pages/Auth/LoginTesting";
+import LoginTesting from "pages/Auth/LoginTesting";
 
 const routes = [
 	{
@@ -60,10 +60,10 @@ const routes = [
 		path: "/login/:message?",
 		component: LoginPage
 	},
-	// {
-	//   path: "/login-testing/:message?",
-	//   component: LoginTesting
-	// },
+	{
+	  path: "/login-testing/:message?",
+	  component: LoginTesting
+	},
 	{
 		path: "/detail/:id",
 		component: DetailProductPage,
