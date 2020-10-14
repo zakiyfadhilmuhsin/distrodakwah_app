1466b5242ea02e93a9504a9588b9adbacc7a2a57
fsidiqs <fsidiqs@hotmail.co.id>
2020-10-14 08:30:01 +0700

    changed main api

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
-const API = 'https://api.prodakwah.id';
+const API = 'https://phpstack-372087-1163645.cloudwaysapps.com';
 const API_DEV = 'http://apigateway.test';
 const WEBREPLICA_DOMAIN = 'https://prodakwah.com';
 const WEBREPLICA_DOMAIN_DEV = 'http://localhost:8082';
