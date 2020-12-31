60f4946724a822fa3e98477f60810427d650810f
fsidiqs <fsidiqs@hotmail.co.id>
2020-12-31 22:50:50 +0700

    ok

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
-const API = 'https://phpstack-372087-1163645.cloudwaysapps.com';
+const API = 'https://phpstack-372088-1163655.cloudwaysapps.com';
 const API_DEV = 'https://phpstack-372088-1163655.cloudwaysapps.com';
 const WEBREPLICA_DOMAIN = 'https://prodakwah.com';
 const WEBREPLICA_DOMAIN_DEV = 'http://localhost:8082';
