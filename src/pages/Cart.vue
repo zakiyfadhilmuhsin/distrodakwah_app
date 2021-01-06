d85feceaf68e94cf17c63ac8c1eaaf88c0c299f8
Fajar Sidiq Salviro <f.sidiq@aryo.jp>
2021-01-06 16:56:32 +0700

    fix cart bugs

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,83 +1,91 @@
 <template>
-	<MainLayout
-		:HeaderProp="'Keranjang Belanja'"
-		v-if="Object.keys(globalState.userProfile).length > 0"
-	>
-		<template v-slot:main>
-			<q-page class="bg-white">
-				<div class="bg-grey-3" style="height: 100%">
-					<ProductList
-						v-if="cartData"
-						:productDataProp="cartData"
-						@getCartData="getCartData"
-					/>
+   <MainLayout
+      :HeaderProp="'Keranjang Belanja'"
+      v-if="Object.keys(globalState.userProfile).length > 0"
+   >
+      <template v-slot:main>
+         <q-page class="bg-white">
+            <div class="bg-grey-3" style="height: 100%">
+               <ProductList
+                  v-if="cartData"
+                  :productDataProp="cartData"
+                  @getCartData="getCartData"
+               />
 
-					<div
-						style="background-color: white; margin-bottom: 5px"
-						v-if="cartData.cart_detail.length > 0"
-					>
-						<div
-							class="row q-px-lg items-center"
-							style="padding-top: 15px; padding-bottom: 15px"
-						>
-							<div class="col">
-								<h6
-									style="font-size: 16px; margin: 0; font-family: 'Open Sans'"
-								>
-									Total Item
-								</h6>
-								<h6
-									style="font-size: 21px; margin: -5px 0 0 0; font-family: 'Open Sans'; font-weight: bold"
-								>
-									{{ allowOrder ? totalItem : 0 }}
-								</h6>
-							</div>
-							<div class="col">
-								<h6
-									style="font-size: 16px; margin: 0; font-family: 'Open Sans'"
-								>
-									Subtotal
-								</h6>
-								<h6
-									style="font-size: 21px; margin: -5px 0 0 0; font-family: 'Open Sans'; font-weight: bold"
-									class="text-red"
-								>
-									Rp {{ allowOrder ? formatPrice(cartData.total_amount) : 0 }}
-								</h6>
-							</div>
-						</div>
-					</div>
-					<div
-						style="background-color: white; margin-bottom: 5px"
-						v-if="cartData.cart_detail.length > 0"
-					>
-						<div
-							class="row q-px-lg items-center"
-							style="padding-top: 15px; padding-bottom: 15px"
-						>
-							<div class="col">
-								<img src="~/assets/images/components/peti.png" width="130" />
-							</div>
-							<div class="col">
-								<h6
-									style="font-size: 12px; margin: 0; font-family: 'Open Sans'; line-height: 18px"
-								>
-									Alhamdulillah potensi keuntungan kamu adalah
-								</h6>
-								<h6
-									style="font-size: 28px; margin: 8px 0; font-family: 'Open Sans'; font-weight: bold"
-								>
-									Rp {{ allowOrder ? formatPrice(totalProfit) : 0 }}
-								</h6>
-								<h6
-									style="font-size: 12px; margin: 0; font-family: 'Open Sans'; line-height: 18px"
-								>
-									Jangan lupa sedekah ya :)
-								</h6>
-							</div>
-						</div>
-					</div>
-					<!-- <div style="background-color: white; margin-bottom: 5px">
+               <div
+                  style="background-color: white; margin-bottom: 5px"
+                  v-if="cartData.cart_detail.length > 0"
+               >
+                  <div
+                     class="row q-px-lg items-center"
+                     style="padding-top: 15px; padding-bottom: 15px"
+                  >
+                     <div class="col">
+                        <h6
+                           style="font-size: 16px; margin: 0; font-family: 'Open Sans'"
+                        >
+                           Total Item
+                        </h6>
+                        <h6
+                           style="font-size: 21px; margin: -5px 0 0 0; font-family: 'Open Sans'; font-weight: bold"
+                        >
+                           {{ allowOrder ? totalItem : 0 }}
+                        </h6>
+                     </div>
+                     <div class="col">
+                        <h6
+                           style="font-size: 16px; margin: 0; font-family: 'Open Sans'"
+                        >
+                           Subtotal
+                        </h6>
+                        <h6
+                           style="font-size: 21px; margin: -5px 0 0 0; font-family: 'Open Sans'; font-weight: bold"
+                           class="text-red"
+                        >
+                           Rp
+                           {{
+                              allowOrder
+                                 ? formatPrice(cartData.total_amount)
+                                 : 0
+                           }}
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div
+                  style="background-color: white; margin-bottom: 5px"
+                  v-if="cartData.cart_detail.length > 0"
+               >
+                  <div
+                     class="row q-px-lg items-center"
+                     style="padding-top: 15px; padding-bottom: 15px"
+                  >
+                     <div class="col">
+                        <img
+                           src="~/assets/images/components/peti.png"
+                           width="130"
+                        />
+                     </div>
+                     <div class="col">
+                        <h6
+                           style="font-size: 12px; margin: 0; font-family: 'Open Sans'; line-height: 18px"
+                        >
+                           Alhamdulillah potensi keuntungan kamu adalah
+                        </h6>
+                        <h6
+                           style="font-size: 28px; margin: 8px 0; font-family: 'Open Sans'; font-weight: bold"
+                        >
+                           Rp {{ allowOrder ? formatPrice(totalProfit) : 0 }}
+                        </h6>
+                        <h6
+                           style="font-size: 12px; margin: 0; font-family: 'Open Sans'; line-height: 18px"
+                        >
+                           Jangan lupa sedekah ya :)
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <!-- <div style="background-color: white; margin-bottom: 5px">
 						<div class="row q-px-lg items-center" style="padding-top: 15px; padding-bottom: 15px">
 							<div class="col">
 								<q-checkbox keep-color color="orange-8" dense v-model="donasi_act"><h6 style="font-size: 12px; margin: 0; font-family: 'Open Sans'; line-height: 18px">Donasi <span class="text-bold">Rp 300</span> melalui lembaga ACT</h6></q-checkbox>
@@ -85,196 +93,211 @@
 							</div>
 						</div>
           </div>-->
-				</div>
-				<div
-					class="row bg-white q-pa-md"
-					v-if="cartData.cart_detail.length > 0"
-				>
-					<div class="col">
-						<q-btn
-							flat
-							:disable="!allowOrder"
-							class="bg-orange-8 text-white full-width"
-							@click="setShippingAddress"
-							>Atur Alamat Pengiriman</q-btn
-						>
-					</div>
-				</div>
-			</q-page>
-		</template>
-	</MainLayout>
+            </div>
+            <div
+               class="row bg-white q-pa-md"
+               v-if="cartData.cart_detail.length > 0"
+            >
+               <div class="col">
+                  <q-btn
+                     flat
+                     :disable="!allowOrder"
+                     class="bg-orange-8 text-white full-width"
+                     @click="setShippingAddress"
+                     >Atur Alamat Pengiriman</q-btn
+                  >
+               </div>
+            </div>
+         </q-page>
+      </template>
+   </MainLayout>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState } from 'vuex';
 import {
-	cartService,
-	catalogService,
-	getCartUrl,
-	catalogProductUrl,
-	removeProductCartUrl,
-	totalCartItemUrl,
-	updateCartQtyUrl,
-	getHeader
-} from "src/config";
+   cartService,
+   catalogService,
+   getCartUrl,
+   catalogProductUrl,
+   removeProductCartUrl,
+   totalCartItemUrl,
+   updateCartQtyUrl,
+   getHeader,
+} from 'src/config';
 // Loading
-import { QSpinnerPuff } from "quasar";
+import { QSpinnerPuff } from 'quasar';
 // components
-import MainLayout from "../layouts/MainLayout.vue";
-import ProductList from "../components/Cart/ProductList.vue";
+import MainLayout from '../layouts/MainLayout.vue';
+import ProductList from '../components/Cart/ProductList.vue';
 
 export default {
-	name: "CartPage",
-	components: { MainLayout, ProductList },
-	data() {
-		return {
-			// Data Keranjang/Cart
-			cartData: {
-				cart_detail: [],
-				total_amount: 0
-			},
-			totalProfit: 0,
-			totalItem: 0,
-			// totalCartItem: 0,
-			// Donasi
-			donasi_act: false,
-			donasi_rq: false,
+   name: 'CartPage',
+   components: { MainLayout, ProductList },
+   data() {
+      return {
+         // Data Keranjang/Cart
+         cartData: {
+            cart_detail: [],
+            total_amount: 0,
+         },
+         totalProfit: 0,
+         totalItem: 0,
+         // totalCartItem: 0,
+         // Donasi
+         donasi_act: false,
+         donasi_rq: false,
 
-			//guard
-			allowOrder: false
-		};
-	},
-	computed: {
-		...mapState(["globalState"])
-	},
-	async created() {
-		if (Object.keys(this.globalState.userProfile).length === 0) {
-			await this.$store.dispatch("globalState/getUserProfile");
-		}
+         //guard
+         allowOrder: false,
+      };
+   },
+   computed: {
+      ...mapState(['globalState']),
+   },
+   async created() {
+      if (Object.keys(this.globalState.userProfile).length === 0) {
+         await this.$store.dispatch('globalState/getUserProfile');
+      }
 
-		await this.getCartData();
-	},
-	methods: {
-		calculateProfit() {
-			let tempProfit = 0;
-			this.cartData.cart_detail.forEach(cartDetail => {
-				tempProfit +=
-					(cartDetail.retail_price - cartDetail.reseller_price) *
-					cartDetail.qty;
-			});
-			this.totalProfit = tempProfit;
-		},
-		async getCartData() {
-			// set allow order to false initially
-			this.$q.loading.show({
-				spinner: QSpinnerPuff,
-				spinnerColor: "black",
-				spinnerSize: 50,
-				backgroundColor: "grey",
-				message: "<b>Mohon Tunggu..</b>",
-				messageColor: "black"
-			});
-			this.allowOrder = false;
+      await this.getCartData();
+   },
+   methods: {
+      calculateProfit() {
+         let tempProfit = 0;
+         this.cartData.cart_detail.forEach((cartDetail) => {
+            tempProfit +=
+               (cartDetail.retail_price - cartDetail.reseller_price) *
+               cartDetail.qty;
+         });
+         this.totalProfit = tempProfit;
+      },
+      async getCartData() {
+         // set allow order to false initially
+         this.$q.loading.show({
+            spinner: QSpinnerPuff,
+            spinnerColor: 'black',
+            spinnerSize: 50,
+            backgroundColor: 'grey',
+            message: '<b>Mohon Tunggu..</b>',
+            messageColor: 'black',
+         });
+         this.allowOrder = false;
 
-			let tempCart,
-				tempProduct,
-				tempProductSkuArr,
-				cartRes,
-				productRes,
-				productSkuRes,
-				tempTotalItem = 0;
+         let tempCart,
+            tempProduct,
+            tempProductSkuArr,
+            cartRes,
+            productRes,
+            productSkuRes,
+            tempTotalItem = 0;
 
-			try {
-				cartRes = await this.$axios({
-					method: "get",
-					url: `${cartService}/get-cart/${this.globalState.userProfile.id}`,
-					headers: getHeader()
-				});
-			} catch (error) {
-				console.log("error fetching cart");
-				console.log(error.message);
-			}
-			tempCart = cartRes.data.data;
+         try {
+            cartRes = await this.$axios({
+               method: 'get',
+               url: `${cartService}/get-cart/${this.globalState.userProfile.id}`,
+               headers: getHeader(),
+            });
+         } catch (error) {
+            console.log('error fetching cart');
+            console.log(error.message);
+         }
+         tempCart = cartRes.data.data;
+         if (
+            tempCart && // cart has been created
+            tempCart.cart_detail.length > 0
+         ) {
+            if (tempCart.voucher_id !== null) {
+               this.couponUse = true;
+               this.couponCode = tempCart.voucher_code_name;
+            }
 
-			if (
-				tempCart && // cart has been created
-				tempCart.cart_detail.length > 0
-			) {
-				if (tempCart.voucher_id !== null) {
-					this.couponUse = true;
-					this.couponCode = tempCart.voucher_code_name;
-				}
+            try {
+               productSkuRes = await this.$axios({
+                  method: 'post',
+                  url: `${catalogService}/get-product-skus-by-id`,
+                  headers: getHeader(),
+                  data: {
+                     productSkuIdArr: tempCart.cart_detail.map(
+                        (e) => e.product_sku_id
+                     ),
+                     select: [
+                        'id',
+                        'stock_qty',
+                        'keep_stock_qty',
+                        'product_id',
+                     ],
+                     eagerLoad: {
+                        productParent: ['id', 'product_name', 'featured_image'],
+                     },
+                  },
+               });
+            } catch (error) {
+               console.log('error fetching product skus');
+            }
 
-				try {
-					productSkuRes = await this.$axios({
-						method: "post",
-						url: `${catalogService}/get-product-skus-by-id`,
-						headers: getHeader(),
-						data: {
-							productSkuIdArr: tempCart.cart_detail.map(e => e.product_sku_id),
-							select: ["id", "stock_qty", "keep_stock_qty", "product_id"],
-							eagerLoad: {
-								productParent: ["id", "product_name", "featured_image"]
-							}
-						}
-					});
-				} catch (error) {}
+            tempProductSkuArr = productSkuRes.data.data;
+            console.log(tempProductSkuArr, 'aaaaaa');
 
-				tempProductSkuArr = productSkuRes.data.data;
+            tempCart.cart_detail.forEach((cart_detail, index) => {
+               tempTotalItem += cart_detail.qty;
 
-				tempCart.cart_detail.forEach((cart_detail, index) => {
-					tempTotalItem += cart_detail.qty;
+               const findProductSku = tempProductSkuArr.find(
+                  (productSku) => productSku.id === cart_detail.product_sku_id
+               );
+               tempCart.cart_detail[
+                  index
+               ].product_name = findProductSku.product_parent
+                  ? findProductSku.product_parent.product_name
+                  : 'produk tidak ada';
 
-					const findProductSku = tempProductSkuArr.find(
-						productSku => productSku.id === cart_detail.product_sku_id
-					);
-					tempCart.cart_detail[index].product_name =
-						findProductSku.product_parent.product_name;
+               tempCart.cart_detail[
+                  index
+               ].featured_image = findProductSku.product_parent
+                  ? findProductSku.product_parent.featured_image
+                  : 'produk tidak ada';
+               tempCart.cart_detail[index].options = JSON.parse(
+                  tempCart.cart_detail[index].options
+               );
 
-					tempCart.cart_detail[index].featured_image =
-						findProductSku.product_parent.featured_image;
-					tempCart.cart_detail[index].options = JSON.parse(
-						tempCart.cart_detail[index].options
-					);
+               // findProductSku.qty - findproductSku.Keep - cart qty
+               tempCart.cart_detail[index].stock_sufficient =
+                  findProductSku.stock_qty - findProductSku.keep_stock_qty <
+                  cart_detail.qty || !findProductSku.product_parent
+                     ? false
+                     : true;
+            });
 
-					// findProductSku.qty - findproductSku.Keep - cart qty
-					tempCart.cart_detail[index].stock_sufficient =
-						findProductSku.stock_qty - findProductSku.keep_stock_qty <
-						cart_detail.qty
-							? false
-							: true;
-				});
+            this.cartData = tempCart;
+            this.allowOrder = !tempCart.cart_detail.some(
+               (productSku) => productSku.stock_sufficient === false
+            );
+         } else {
+            this.cartData = {
+               cart_detail: [],
+               total_amount: 0,
+            };
+         }
+         this.totalItem = tempTotalItem;
+         this.calculateProfit();
+         this.$q.loading.hide();
+      },
+      formatPrice(value) {
+         let val = (value / 1).toFixed(0).replace('.', ',');
+         return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+      },
 
-				this.cartData = tempCart;
-				this.allowOrder = !tempCart.cart_detail.some(
-					productSku => productSku.stock_sufficient === false
-				);
-			} else {
-				this.cartData = {
-					cart_detail: [],
-					total_amount: 0
-				};
-			}
-			this.totalItem = tempTotalItem;
-			this.calculateProfit();
-			this.$q.loading.hide();
-		},
-		formatPrice(value) {
-			let val = (value / 1).toFixed(0).replace(".", ",");
-			return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-		},
-
-		async setShippingAddress() {
-			await this.getCartData();
-			if (this.allowOrder) {
-				this.$router.push({
-					name: "Shipping"
-				});
-			} else {
-				alert("Keranjang Belanja Masih Kosong!");
-			}
-		}
-	}
+      async setShippingAddress() {
+         await this.getCartData();
+         if (this.allowOrder) {
+            this.$router.push({
+               name: 'Shipping',
+            });
+         } else {
+            alert('Keranjang Belanja Masih Kosong!');
+         }
+      },
+   },
 };
 </script>
 
